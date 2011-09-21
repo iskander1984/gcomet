314ff767671ffadf856a92dde73df214aac0c445
Pochapskiy Olexandr <olexandr.pochapskiy@gmail.com>
2011-09-21 17:40:39 +0300

    Fixed paths in GcometTaglib. Added jquery as direct dependency

--- grails-app/taglib/org/grails/plugin/gcomet/GcometTagLib.groovy
+++ grails-app/taglib/org/grails/plugin/gcomet/GcometTagLib.groovy
@@ -4,12 +4,13 @@ class GcometTagLib {
 	static namespace = "gcomet"
 	
 	def includes = { attrs, body ->
-		out << "<script type=\"text/javascript\" src=\"/grails-comet-gpars/js/gcomet.js\"></script>" 
-		out << "<script type=\"text/javascript\" src=\"/grails-comet-gpars/js/jquery/jquery.json-2.2.min.js\"></script>" 
+		out << "<script type=\"text/javascript\" src=\"js/jquery/jquery-1.6.1.js\"></script>" 
+		out << "<script type=\"text/javascript\" src=\"js/gcomet.js\"></script>" 
+		out << "<script type=\"text/javascript\" src=\"js/jquery/jquery.json-2.2.min.js\"></script>" 
 	}
 	
 	def component = { attrs, body ->
 		out << "<span id='${attrs.id}' type='gcomet' updateHandler='${attrs.updateHandler}'>" + body() + "</span>"
 	}
 
-}
+} 
